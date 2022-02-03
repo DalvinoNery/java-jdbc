42da2ed5703fe0a5b3a7cb2d992981cf0def4d07
DalvinoNery <rodrigo.s.nery@gmail.com>
2022-02-03 20:08:30 -0300

    Aula 02 Assumindo o controle  - desabilitando o autocommit.

---
+++ src/TesteListagem.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class TesteListagem {
+
+    public static void main(String [] args) throws SQLException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        Connection connection = connectionFactory.recuperaConexao();
+        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM produto");
+        pstm.execute();
+        ResultSet rst = pstm.getResultSet();
+
+        while (rst.next()){
+            int id = rst.getInt("ID");
+            System.out.println("ID - "+ id);
+            String nome = rst.getString("nome");
+            System.out.println("nome - "+ nome);
+            String descricao = rst.getString("descricao");
+            System.out.println("descrição - "+ descricao);
+        }
+
+        connection.close();
+    }
+}
