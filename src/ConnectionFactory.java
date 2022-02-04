aa8b0d083bf23a143ff0e68fff973f772795c375
DalvinoNery <rodrigo.s.nery@gmail.com>
2022-02-03 22:20:22 -0300

    - Determinando quantidade máxima de conexões no pool.

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,24 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+    public DataSource dataSource;
+    /*Configuração do pool de conexão*/
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UCT");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("12345678");
+        comboPooledDataSource.setMaxPoolSize(15);/*Quantidade máxima de conexões*/
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recuperaConexao() throws SQLException {
-        return DriverManager
-                .getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UCT","root","12345678");
+        return this.dataSource.getConnection();
 
     }
 }
